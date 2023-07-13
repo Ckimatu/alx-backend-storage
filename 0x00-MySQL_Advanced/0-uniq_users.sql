2b9669e3cd91b2ab0d322e500cfbdf8774bc72d5
Caroline <carolinekimatu6@gmail.com>
2023-07-13 08:31:18 +0000

    syntax error fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -5,7 +5,8 @@
 -- If the table already exists, your script should not fail
 -- script can be executed on any database
 CREATE TABLE IF NOT EXISTS users (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
+
