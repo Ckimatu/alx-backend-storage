982a5c39a7e1b3b8aa8c794f9edf8ba39c206ea9
Caroline <carolinekimatu6@gmail.com>
2023-07-19 11:30:24 +0000

    writing to and reading from redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Writing strings to Redis
+Reading from Redis and recovering original type
+"""
+
+import redis
+import uuid
+from typing import Union, Callable
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[
+            str, bytes, int, float, None]:
+        data = self._redis.get(key)
+        if data is not None:
+            if fn is not None:
+                data = fn(data)
+            return data
+        return None
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=int)
